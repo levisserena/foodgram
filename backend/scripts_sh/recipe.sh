fa9433f41efa3fea85a745647e08f93ae42dc904
Lev Akchurin <levisserena@yandex.ru>
2024-08-15 23:00:20 +0400

    Правка скрипта с рецептами

--- backend/scripts_sh/recipe.sh
+++ backend/scripts_sh/recipe.sh
@@ -20,32 +20,29 @@ Recipe.objects.bulk_create([
         image=f'recipes/images/test_r{i}.jpg'
     ) for i in range(1, NUMBER_RECIPE + 1)
 ])
-recipe_id = Recipe.objects.values_list('id', flat=True)
-ing_id_list = Ingredient.objects.values_list('id', flat=True)
-tag_id_list = Tag.objects.values_list('id', flat=True)
-def get_ing(): return sample([i for i in ing_id_list], randint(1, 4))
-def get_tag(): return sample([i for i in tag_id_list], randint(1, 2))
-def rec_ing():
-    ing_id = get_ing()
-    for i_id in ing_id:
+recipe_id = list(Recipe.objects.values_list('id', flat=True))
+ing_id_list = list(Ingredient.objects.values_list('id', flat=True))
+tag_id_list = list(Tag.objects.values_list('id', flat=True))
+def get_ing(): return sample(ing_id_list, randint(1, 4))
+def get_tag(): return sample(tag_id_list, randint(1, 2))
+def rec_ing_tag():
+    for r_id in recipe_id:
+        ing_id = get_ing()
         RecipeIngredient.objects.bulk_create([
             RecipeIngredient(
                 recipe=Recipe.objects.get(id=r_id),
                 ingredient=Ingredient.objects.get(id=i_id),
                 amount=randrange(10, 2000, 10),
-            ) for r_id in recipe_id
+            ) for i_id in ing_id
         ])
-def rec_tag():
-    tag_id = get_ing()
-    for t_id in tag_id:
+        tag_id = get_tag()
         RecipeTag.objects.bulk_create([
             RecipeTag(
                 recipe=Recipe.objects.get(id=r_id),
                 tag=Tag.objects.get(id=t_id),
-            ) for r_id in recipe_id
+            ) for t_id in tag_id
         ])
-rec_ing()
-rec_tag()
+rec_ing_tag()
 count = Recipe.objects.all().count()
 print(f'Total entries made: {count}.')
 "
